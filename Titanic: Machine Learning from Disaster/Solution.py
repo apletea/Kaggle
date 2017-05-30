b93bc3a8bf90e5cf0d80da60162f3078fdacdc9d
Dzmitry Kamarouski <dzkamarouski@gmail.com>
2017-05-30 08:40:59 +0300

    almost done

--- Titanic: Machine Learning from Disaster/Solution.py
+++ Titanic: Machine Learning from Disaster/Solution.py
@@ -1,27 +1,44 @@
 import numpy as np
 import pandas
-import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeClassifier
+
+def import_data(str,parametr):
+    return pandas.read_csv(str, index_col = parametr)
+
+def test():
+    X = np.array([[1, 2], [3, 4], [5, 6]])
+    y = np.array([0, 1, 0])
+    clf = DecisionTreeClassifier()
+    clf.fit(X, y)
+    importances = clf.feature_importances_
+    print(importances)
+
+
 
 
 def main():
-    data = pandas.read_csv('train.csv',index_col = 'PassengerId')
-    data_to_let = data.loc[:, ['Survived','Pclass','Sex','Age','SibSp','Parch','Fare','Cabin']]
+    data = import_data('train.csv','PassengerId')
+    data_to_let = data.loc[ : ,['Pclass', 'Fare', 'Age', 'Sex', 'Survived']]
     data2 = data_to_let.dropna()
     SexN = data2.Sex.factorize()
-    data2['SexN'] = SexN[0]
-    X = data2.loc[:, ['Pclass','SexN','Age','SibSp','Parch','Fare','Cabin']]
+    data2.loc[:,'SexN'] = SexN[0]
+    X = data2.loc[ :, ['Pclass', 'Fare' , 'Age', 'SexN']]
     y = data2['Survived']
-    clf = DecisionTreeClassifier(random_state=241)
-    clf.fit(X, y)
+    clf = DecisionTreeClassifier()
+    clf.random_state = 241
+    clf.fit(X,y)
 
-
-
-    test_data = pandas.read_csv('test.csv', index_col='PassengerId')
-    test_data_to_let = test_data.loc[:, ['Pclass','Sex','Age','SibSp','Parch','Fare','Cabin']]
-    test_data2 =  test_data_to_let.dropna()
-    test_SexN = test_data2.Sex.factorize()
-    test_data2['SexN'] = test_SexN[0]
-    X = test_data2.loc[:, ['Pclass','SexN','Age','SibSp','Parch','Fare','Cabin']]
+    test_data = import_data('test.csv','PassengerId')
+    test_to_let = test_data.loc[ : ,['Pclass', 'Fare', 'Age', 'Sex']]
+    SexN = data2.Sex.factorize()
+    data2.loc[:, 'SexN'] = SexN[0]
+    X = data2.loc[ :, ['Pclass', 'Fare' , 'Age', 'SexN']]
     res = clf.predict(X)
+   
 main()
+
+
+
+
+
+
