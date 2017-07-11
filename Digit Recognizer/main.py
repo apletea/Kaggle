7085802b512c43df5bfb4074825c1cc5e5c0f492
Dzmitry Kamarouski <dzkamarouski@gmail.com>
2017-07-11 23:19:48 +0300

    kaggle

--- Digit Recognizer/main.py
+++ Digit Recognizer/main.py
@@ -1,34 +1,62 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 import pylab
-import matplotlib.pyplot as plt, matplotlib.image as mpimg
+from keras.models import Sequential
+from keras.layers import Dense,Dropout,Lambda,Flatten
+from keras.optimizers import Adam,RMSprop
 from sklearn.model_selection import train_test_split
-from sklearn import svm
+from keras.models import  Sequential
+from keras.layers.core import  Lambda , Dense, Flatten, Dropout
+from keras.callbacks import EarlyStopping
+from keras.layers import BatchNormalization, Convolution2D , MaxPooling2D
+from subprocess import check_output
+from keras.preprocessing import image
+test = pd.read_csv('test.csv')
+train = pd.read_csv('train.csv')
 
-labeled_images = pd.read_csv('train.csv')
-images = labeled_images.iloc[:,1:]
-labels = labeled_images.iloc[:,:1]
-train_images, test_images,train_labels, test_labels = train_test_split(images, labels, train_size=0.99, random_state=0)
+X_train = (train.ix[:,1:].values).astype('float32')
+Y_train = train.ix[:,0].values.astype('int32')
+X_test = test.values.astype('float32')
 
-test_images[test_images>0]=1
-train_images[train_images>0]=1
-i=1
-img=train_images.iloc[i].as_matrix().reshape((28,28))
-plt.imshow(img,cmap='binary')
-plt.title(train_labels.iloc[i])
-plt.hist(train_images.iloc[i])
+X_train = X_train.reshape(X_train.shape[0],28,28)
+
+for i in range(6, 9):
+    plt.subplot(330 + (i+1))
+    plt.imshow(X_train[i], cmap=plt.get_cmap('gray'))
+    plt.title(Y_train[i])
+
+X_train = X_train.reshape(X_train.shape[0], 28, 28,1)
+
+X_test = X_test.reshape(X_test.shape[0], 28, 28,1)
+
+mean_px = X_train.mean().astype(np.float32)
+std_px = X_train.std().astype(np.float32)
+
+def standardize(x):
+    return (x-mean_px)/std_px
+
+
+
+
+from keras.utils.np_utils import to_categorical
+y_train= to_categorical(Y_train)
+num_classes = y_train.shape[1]
+
+plt.title(y_train[9])
+plt.plot(y_train[9])
+plt.xticks(range(10))
+
+seed = 43
+np.random.seed(seed)
+
 pylab.show()
 
+model=Sequential()
+model.add(Lambda(standardize,input_shape=(28,28,1)))
+model.add(Flatten())
+model.add(Dense(10,activation='softmax'))
+model.compile(optimizer=RMSprop(lr=0.001),loss='categorical_crossentropy',metrics=['accuracy'])
 
-clf = svm.SVC()
-clf.fit(train_images, train_labels.values.ravel())
-print(clf.score(test_images,test_labels))
+gen = image.ImageDataGenerator()
 
-test_data = pd.read_csv('test.csv')
-test_data[test_data>0]=1
-resulrt = clf.predict(test_data[:])
-
-df = pd.DataFrame(resulrt)
-df.index.name='ImageId'
-df.index+=1
-df.columns=['Label']
-df.to_csv('results.csv', header=True)
