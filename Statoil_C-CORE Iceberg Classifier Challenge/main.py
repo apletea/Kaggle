872f0294a54963329d0544251aaf15e89240827b
Dzmitry Kamarouski <dzkamarouski@gmail.com>
2018-01-08 01:31:03 +0300

    Update main.py

--- Statoil_C-CORE Iceberg Classifier Challenge/main.py
+++ Statoil_C-CORE Iceberg Classifier Challenge/main.py
@@ -1,16 +1,27 @@
-import shutil
 import matplotlib.pyplot as plt
 from keras.applications.vgg19 import VGG19
 from keras.preprocessing import image
 from keras.layers import Dense, GlobalAveragePooling2D, Dropout, Activation
 from keras.models import Model, load_model
 from keras.applications.vgg19 import preprocess_input,decode_predictions
-from sklearn.model_selection import ParameterGrid, StratifiedKFold
 import numpy as np
+from scipy import signal
+from keras.optimizers import SGD
 from keras_tqdm import TQDMNotebookCallback
 from keras.callbacks import ModelCheckpoint, EarlyStopping
 from tqdm import tqdm_notebook
 from keras.preprocessing.image import ImageDataGenerator
+import xgboost
+import os
+import pandas
+import cv2
+from keras.layers import *
+from keras.models import *
+import pandas as pd
+from tqdm import tqdm
+import cv2
+
+
 def get_VGG19():
 
     model = VGG19(weights='imagenet',include_top=False,input_shape=(75, 75, 3))
@@ -50,11 +61,12 @@ def pop_last_3_layers(model):
     pop(model)
     return model
 
-def export_weight(model,filepath):
-    model.save_weights(filepath)
+def export_model(model,filepath):
+    model.save(filepath)
 
 def import_weight(model, filepath):
-    model.weights(filepath, by_name=False)
+    model = load_model(filepath)
+    return model
 
 
 def calc_mean_std(X):
@@ -74,88 +86,44 @@ def preprocess_poster(x, train_mean, train_std):
     x /= train_std
     return x
 
-data = pd.read_json('fast.json')
-data.inc_angle = data.inc_angle.apply(lambda x: np.nan if x == 'na' else x)
-X_band_1=np.array([np.array(band).astype(np.float32).reshape(75, 75) for band in data["band_1"]])
-X_band_2=np.array([np.array(band).astype(np.float32).reshape(75, 75) for band in data["band_2"]])
-X_band_3=(X_band_1+X_band_2)/2
-imgs =  np.concatenate([X_band_1[:, :, :, np.newaxis]
-                          , X_band_2[:, :, :, np.newaxis]
-                         , X_band_3[:, :, :, np.newaxis]], axis=-1)
-X,y =data.drop(['is_iceberg','band_2','band_1','id'], axis=1).as_matrix(), data['is_iceberg'].values
-print X
-n_splits = (StratifiedKFold(n_splits=5))
-n_splits.get_n_splits(X,y)
-train_stat = []
+im_size = 75
 
-batch_size = 32
-checkpoints_prefix = './check/e/'
+data = pd.read_json('train.json')
+print len(data)
+X = []
+y = []
+data.inc_angle = data.inc_angle.apply(lambda x: -1 if x == 'na' else x)
+for i in xrange(0,len(data)):
+    img = cv2.imread('./band_res/{}.jpg'.format(data.id[i]))
+    if data.inc_angle[i] != -1:
+        X.append(img)
+        y.append(data.is_iceberg[i])
+print y
+y = np.transpose(y,axes=-1)
+print y
+model = Sequential()
+model.add(Convolution2D(50,11,4,input_shape=(75,75,3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D((3,3),2))
+model.add(Convolution2D(100,5,1))
+model.add(Activation('relu'))
+model.add(MaxPooling2D((3,3),2))
+model.add(Convolution2D(200,3,1))
+model.add(Activation('relu'))
+model.add(MaxPooling2D((3,3),2))
+model.add(Dropout(0.5))
+model.add(Flatten())
+model.add(Dense(100))
+model.add(Activation('selu'))
+model.add(Dropout(0.5))
+model.add(Dense(200))
+model.add(Activation('selu'))
+model.add(Dropout(0.5))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer='adam')
 
-for fold_i, (train_idxs, test_idxs) in tqdm_notebook(list(enumerate(n_splits))):
-    fold_stat = {}
-    y_train, y_test = y[train_idxs], y[test_idxs]
-    train_posters, test_posters = imgs[train_idxs], imgs[test_idxs]
-    train_mean, train_std = calc_mean_std(train_posters)
-    fold_stat['posters_train_mean'] = train_mean
-    fold_stat['posters_train_std'] = train_std
+X = np.array(X,np.float32)/255
 
-    X_train_posters = np.array([preprocess_poster(poster_i, train_mean, train_std)
-                               for poster_i in train_posters])
-
-    train_datagen = ImageDataGenerator(
-        shear_range=0.4,
-        horizontal_flip=True,
-        rotation_range=20.,
-        width_shift_range=0.4,
-        height_shift_range=0.4,
-        zoom_range=0.4,
-        vertical_flip=True,
-    )
-
-    train_datagen.fit(X_train_posters, augment=False)
-    train_flow = train_datagen.flow(X_train_posters, y_train, batch_size=batch_size)
-
-    X_test_posters = np.array([preprocess_poster(poster_i, train_mean, train_std)
-                               for poster_i in test_posters])
-
-    steps_per_epoch = len(X_train_posters) // batch_size
-    model = get_VGG19()
-
-    for layer in model.layers[:-5]:
-        layer.trainable = False
-    for layer in model.layers[-5:]:
-        layer.trainable = True
-
-    model.compile(optimizer='rmsprop', loss='binary_crossentropy',metrics=['accuracy'])
-    base_checkpoints_path = os.path.join(checkpoints_prefix, 'base_checkpoints', str(fold_i))
-    checkpoints_path = os.path.join(checkpoints_prefix, 'checkpoints', str(fold_i))
-
-
-    shutil.rmtree(base_checkpoints_path, ignore_errors=True)
-    shutil.rmtree(checkpoints_path, ignore_errors=True)
-
-    os.makedirs(base_checkpoints_path)
-    os.makedirs(checkpoints_path)
-
-    base_checkpoints_path_template = os.path.join(base_checkpoints_path, 'base_checkpoint.hdf5')
-    checkpoints_path_template = os.path.join(checkpoints_path, 'checkpoints.hdf5')
-
-    base_checkpointer = ModelCheckpoint(base_checkpoints_path_template, save_best_only=True)
-    checkpointer = ModelCheckpoint(checkpoints_path_template, save_best_only=True)
-
-    base_history = model.fit_generator(
-        train_flow,
-        steps_per_epoch=steps_per_epoch,
-        validation_data=(X_test_posters, y_test),
-        epochs=10,
-        callbacks=[TQDMNotebookCallback(leave_outer=False), base_checkpointer, EarlyStopping(patience=10)],
-        verbose=0
-    )
-
-    fold_stat['base_history'] = base_history.history
-
-    for layer in model.layers[:15]:
-        layer.trainable = False
-    for layer in model.layers[15:]:
-        layer.trainable = True
-
+print model.summary()
+model.fit(X, y, nb_epoch=100, validation_split=0.1)
+model.save('backup.h5')
