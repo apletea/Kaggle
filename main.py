64295255d3705a7d699b96d634f8aec7876212f7
Dzmitry Kamarouski <dzkamarouski@gmail.com>
2018-05-31 16:06:24 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,324 @@
+import os 
+import pandas as pd
+import cv2
+import numpy as np
+import tensorflow as tf
+import keras
+import random
+
+config = tf.ConfigProto( device_count = {'GPU' : 1, 'CPU':8})
+sess = tf.Session(config=config)
+keras.backend.set_session(sess)
+
+from keras.layers import *
+from keras.models import *
+from keras.optimizers import *
+from keras.applications import *
+from keras.regularizers import *
+from keras.callbacks import ModelCheckpoint
+from keras import backend as K
+from keras.models import load_model
+from keras.utils import plot_model
+
+from keras.layers.core import RepeatVector, Permute
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+
+
+data_ids = pd.read_csv('train.csv', header=None)
+epochs = 200
+size = 512
+batch_size = 16
+def randomShiftScaleRotate(image, mask,
+                           shift_limit=(-0.0625, 0.0625),
+                           scale_limit=(-0.1, 0.1),
+                           rotate_limit=(-45, 45), aspect_limit=(0, 0),
+                           borderMode=cv2.BORDER_CONSTANT, u=0.5):
+    if np.random.random() < u:
+        height, width, channel = image.shape
+
+        angle = np.random.uniform(rotate_limit[0], rotate_limit[1])  # degree
+        scale = np.random.uniform(1 + scale_limit[0], 1 + scale_limit[1])
+        aspect = np.random.uniform(1 + aspect_limit[0], 1 + aspect_limit[1])
+        sx = scale * aspect / (aspect ** 0.5)
+        sy = scale / (aspect ** 0.5)
+        dx = round(np.random.uniform(shift_limit[0], shift_limit[1]) * width)
+        dy = round(np.random.uniform(shift_limit[0], shift_limit[1]) * height)
+
+        cc = np.math.cos(angle / 180 * np.math.pi) * sx
+        ss = np.math.sin(angle / 180 * np.math.pi) * sy
+        rotate_matrix = np.array([[cc, -ss], [ss, cc]])
+
+        box0 = np.array([[0, 0], [width, 0], [width, height], [0, height], ])
+        box1 = box0 - np.array([width / 2, height / 2])
+        box1 = np.dot(box1, rotate_matrix.T) + np.array([width / 2 + dx, height / 2 + dy])
+
+        box0 = box0.astype(np.float32)
+        box1 = box1.astype(np.float32)
+        mat = cv2.getPerspectiveTransform(box0, box1)
+        image = cv2.warpPerspective(image, mat, (width, height), flags=cv2.INTER_LINEAR, borderMode=borderMode,
+                                    borderValue=(
+                                        0, 0,
+                                        0,))
+        mask = cv2.warpPerspective(mask, mat, (width, height), flags=cv2.INTER_LINEAR, borderMode=borderMode,
+                                   borderValue=(
+                                       0, 0,
+                                       0,))
+
+        return image, mask
+    
+def randomHorizontalFlip(image, mask, u=0.5):
+    if np.random.random() < u:
+        image = cv2.flip(image, 1)
+        mask = cv2.flip(mask, 1)
+
+    return image, mask
+
+
+def train_generator():
+    while True:
+        for start in range(0, len(ids_train_split), batch_size):
+            x_batch = []
+            y_batch = []
+            end = min(start + batch_size, len(ids_train_split))
+            ids_train_batch = ids_train_split[start:end]
+            for id in ids_train_batch.values:
+                img = cv2.imread('ISIC2018_Task1-2_Training_Input/{}.jpg'.format(id))
+                img = cv2.resize(img, (input_size, input_size))
+                mask = cv2.imread('ISIC2018_Task1_Training_GroundTruth/{}_segmentation.png'.format(id), cv2.IMREAD_GRAYSCALE)
+                mask = cv2.resize(mask, (input_size, input_size))
+                img = randomHueSaturationValue(img,
+                                               hue_shift_limit=(-50, 50),
+                                               sat_shift_limit=(-5, 5),
+                                               val_shift_limit=(-15, 15))
+                img, mask = randomShiftScaleRotate(img, mask,
+                                                   shift_limit=(-0.0625, 0.0625),
+                                                   scale_limit=(-0.1, 0.1),
+                                                   rotate_limit=(-0, 0))
+                img, mask = randomHorizontalFlip(img, mask)
+                mask = np.expand_dims(mask, axis=2)
+                x_batch.append(img)
+                y_batch.append(mask)
+            x_batch = np.array(x_batch, np.float32) / 255
+            y_batch = np.array(y_batch, np.float32) / 255
+            yield x_batch, y_batch
+    
+    
+def valid_generator():
+    while True:
+        for start in range(0, len(ids_valid_split), batch_size):
+            x_batch = []
+            y_batch = []
+            end = min(start + batch_size, len(ids_valid_split))
+            ids_valid_batch = ids_valid_split[start:end]
+            for id in ids_valid_batch.values:
+                img = cv2.imread('ISIC2018_Task1-2_Training_Input/{}.jpg'.format(id))
+                img = cv2.resize(img, (input_size, input_size))
+                mask = cv2.imread('ISIC2018_Task1_Training_GroundTruth/{}_segmentation.png'.format(id), cv2.IMREAD_GRAYSCALE)
+                mask = cv2.resize(mask, (input_size, input_size))
+                mask = np.expand_dims(mask, axis=2)
+                x_batch.append(img)
+                y_batch.append(mask)
+            x_batch = np.array(x_batch, np.float32) / 255
+            y_batch = np.array(y_batch, np.float32) / 255
+            yield x_batch, y_batch    
+            
+            
+from keras.losses import binary_crossentropy
+import keras.backend as K
+
+
+def dice_coeff(y_true, y_pred):
+    smooth = 1.
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    score = (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+    return score
+
+
+def dice_loss(y_true, y_pred):
+    loss = 1 - dice_coeff(y_true, y_pred)
+    return loss
+
+
+def bce_dice_loss(y_true, y_pred):
+    loss = binary_crossentropy(y_true, y_pred) + dice_loss(y_true, y_pred)
+    return loss
+
+
+
+def get_unet_512(input_shape=(512, 512, 3),
+                 num_classes=1):
+    inputs = Input(shape=input_shape)
+    # 512
+
+    down0a = Conv2D(16, (3, 3), padding='same')(inputs)
+    down0a = BatchNormalization()(down0a)
+    down0a = Activation('relu')(down0a)
+    down0a = Conv2D(16, (3, 3), padding='same')(down0a)
+    down0a = BatchNormalization()(down0a)
+    down0a = Activation('relu')(down0a)
+    down0a_pool = MaxPooling2D((2, 2), strides=(2, 2))(down0a)
+    # 256
+
+    down0 = Conv2D(32, (3, 3), padding='same')(down0a_pool)
+    down0 = BatchNormalization()(down0)
+    down0 = Activation('relu')(down0)
+    down0 = Conv2D(32, (3, 3), padding='same')(down0)
+    down0 = BatchNormalization()(down0)
+    down0 = Activation('relu')(down0)
+    down0_pool = MaxPooling2D((2, 2), strides=(2, 2))(down0)
+    # 128
+
+    down1 = Conv2D(64, (3, 3), padding='same')(down0_pool)
+    down1 = BatchNormalization()(down1)
+    down1 = Activation('relu')(down1)
+    down1 = Conv2D(64, (3, 3), padding='same')(down1)
+    down1 = BatchNormalization()(down1)
+    down1 = Activation('relu')(down1)
+    down1_pool = MaxPooling2D((2, 2), strides=(2, 2))(down1)
+    # 64
+
+    down2 = Conv2D(128, (3, 3), padding='same')(down1_pool)
+    down2 = BatchNormalization()(down2)
+    down2 = Activation('relu')(down2)
+    down2 = Conv2D(128, (3, 3), padding='same')(down2)
+    down2 = BatchNormalization()(down2)
+    down2 = Activation('relu')(down2)
+    down2_pool = MaxPooling2D((2, 2), strides=(2, 2))(down2)
+    # 32
+
+    down3 = Conv2D(256, (3, 3), padding='same')(down2_pool)
+    down3 = BatchNormalization()(down3)
+    down3 = Activation('relu')(down3)
+    down3 = Conv2D(256, (3, 3), padding='same')(down3)
+    down3 = BatchNormalization()(down3)
+    down3 = Activation('relu')(down3)
+    down3_pool = MaxPooling2D((2, 2), strides=(2, 2))(down3)
+    # 16
+
+    down4 = Conv2D(512, (3, 3), padding='same')(down3_pool)
+    down4 = BatchNormalization()(down4)
+    down4 = Activation('relu')(down4)
+    down4 = Conv2D(512, (3, 3), padding='same')(down4)
+    down4 = BatchNormalization()(down4)
+    down4 = Activation('relu')(down4)
+    down4_pool = MaxPooling2D((2, 2), strides=(2, 2))(down4)
+    # 8
+
+    center = Conv2D(1024, (3, 3), padding='same')(down4_pool)
+    center = BatchNormalization()(center)
+    center = Activation('relu')(center)
+    center = Conv2D(1024, (3, 3), padding='same')(center)
+    center = BatchNormalization()(center)
+    center = Activation('relu')(center)
+    # center
+
+    up4 = UpSampling2D((2, 2))(center)
+    up4 = concatenate([down4, up4], axis=3)
+    up4 = Conv2D(512, (3, 3), padding='same')(up4)
+    up4 = BatchNormalization()(up4)
+    up4 = Activation('relu')(up4)
+    up4 = Conv2D(512, (3, 3), padding='same')(up4)
+    up4 = BatchNormalization()(up4)
+    up4 = Activation('relu')(up4)
+    up4 = Conv2D(512, (3, 3), padding='same')(up4)
+    up4 = BatchNormalization()(up4)
+    up4 = Activation('relu')(up4)
+    # 16
+
+    up3 = UpSampling2D((2, 2))(up4)
+    up3 = concatenate([down3, up3], axis=3)
+    up3 = Conv2D(256, (3, 3), padding='same')(up3)
+    up3 = BatchNormalization()(up3)
+    up3 = Activation('relu')(up3)
+    up3 = Conv2D(256, (3, 3), padding='same')(up3)
+    up3 = BatchNormalization()(up3)
+    up3 = Activation('relu')(up3)
+    up3 = Conv2D(256, (3, 3), padding='same')(up3)
+    up3 = BatchNormalization()(up3)
+    up3 = Activation('relu')(up3)
+    # 32
+
+    up2 = UpSampling2D((2, 2))(up3)
+    up2 = concatenate([down2, up2], axis=3)
+    up2 = Conv2D(128, (3, 3), padding='same')(up2)
+    up2 = BatchNormalization()(up2)
+    up2 = Activation('relu')(up2)
+    up2 = Conv2D(128, (3, 3), padding='same')(up2)
+    up2 = BatchNormalization()(up2)
+    up2 = Activation('relu')(up2)
+    up2 = Conv2D(128, (3, 3), padding='same')(up2)
+    up2 = BatchNormalization()(up2)
+    up2 = Activation('relu')(up2)
+    # 64
+
+    up1 = UpSampling2D((2, 2))(up2)
+    up1 = concatenate([down1, up1], axis=3)
+    up1 = Conv2D(64, (3, 3), padding='same')(up1)
+    up1 = BatchNormalization()(up1)
+    up1 = Activation('relu')(up1)
+    up1 = Conv2D(64, (3, 3), padding='same')(up1)
+    up1 = BatchNormalization()(up1)
+    up1 = Activation('relu')(up1)
+    up1 = Conv2D(64, (3, 3), padding='same')(up1)
+    up1 = BatchNormalization()(up1)
+    up1 = Activation('relu')(up1)
+    # 128
+
+    up0 = UpSampling2D((2, 2))(up1)
+    up0 = concatenate([down0, up0], axis=3)
+    up0 = Conv2D(32, (3, 3), padding='same')(up0)
+    up0 = BatchNormalization()(up0)
+    up0 = Activation('relu')(up0)
+    up0 = Conv2D(32, (3, 3), padding='same')(up0)
+    up0 = BatchNormalization()(up0)
+    up0 = Activation('relu')(up0)
+    up0 = Conv2D(32, (3, 3), padding='same')(up0)
+    up0 = BatchNormalization()(up0)
+    up0 = Activation('relu')(up0)
+    # 256
+
+    up0a = UpSampling2D((2, 2))(up0)
+    up0a = concatenate([down0a, up0a], axis=3)
+    up0a = Conv2D(16, (3, 3), padding='same')(up0a)
+    up0a = BatchNormalization()(up0a)
+    up0a = Activation('relu')(up0a)
+    up0a = Conv2D(16, (3, 3), padding='same')(up0a)
+    up0a = BatchNormalization()(up0a)
+    up0a = Activation('relu')(up0a)
+    up0a = Conv2D(16, (3, 3), padding='same')(up0a)
+    up0a = BatchNormalization()(up0a)
+    up0a = Activation('relu')(up0a)
+    # 512
+
+    classify = Conv2D(num_classes, (1, 1), activation='sigmoid')(up0a)
+
+    model = Model(inputs=inputs, outputs=classify)
+
+    model.compile(optimizer=RMSprop(lr=0.0001), loss=bce_dice_loss, metrics=[dice_coeff])
+
+    return model
+    
+callbacks = [EarlyStopping(monitor='val_loss',
+                           patience=8,
+                           verbose=1,
+                           min_delta=1e-4),
+             ReduceLROnPlateau(monitor='val_loss',
+                               factor=0.1,
+                               patience=4,
+                               verbose=1,
+                               epsilon=1e-4),
+             ModelCheckpoint(monitor='val_loss',
+                             filepath='weights/best_weights.hdf5',
+                             save_best_only=True,
+                             save_weights_only=True),
+             TensorBoard(log_dir='logs')]
+
+model.fit_generator(generator=train_generator(),
+                    steps_per_epoch=np.ceil(float(len(ids_train_split)) / float(batch_size)),
+                    epochs=epochs,
+                    verbose=2,
+                    callbacks=callbacks,
+                    validation_data=valid_generator(),
+                    validation_steps=np.ceil(float(len(ids_valid_split)) / float(batch_size)))
